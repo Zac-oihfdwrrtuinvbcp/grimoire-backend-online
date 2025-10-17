4a06a671d25ef8b56e24d85818150d932b2aa995
zac <zak.darmoise1@gmail.com>
2025-10-17 16:38:49 +0200

    Add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
+const cors = require('cors');
 
 const booksRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
@@ -22,13 +23,13 @@ mongoose.connect(`mongodb+srv://${process.env.MONGODB_LOGIN}:${process.env.MONGO
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   next();
+// });
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/books', booksRoutes);
